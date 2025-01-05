41e24d69b5286daff5f9bdd97ae415c27f02761a
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-01-05 14:49:42 +0800

    Update NFFGirlsItemEventListeners.java

--- src/main/java/net/sodiumzh/nff/girls/eventlisteners/NFFGirlsItemEventListeners.java
+++ src/main/java/net/sodiumzh/nff/girls/eventlisteners/NFFGirlsItemEventListeners.java
@@ -20,19 +20,20 @@ import net.sodiumzh.nff.girls.registry.NFFGirlsItems;
 import net.sodiumzh.nff.services.eventlisteners.ServerEntityTickEvent;
 import net.sodiumzh.nff.services.item.NFFMobRespawnerInstance;
 import net.sodiumzh.nff.services.item.NFFMobRespawnerItem;
-import net.sodiumzh.nff.services.item.event.NFFMobRespawnerConstructEvent;
+import net.sodiumzh.nff.services.item.event.NFFMobRespawnerAfterConstructEvent;
+import net.sodiumzh.nff.services.item.event.NFFMobRespawnerBeforeConstructEvent;
 import net.sodiumzh.nff.services.item.event.NFFMobRespawnerStartRespawnEvent;
 
 @Mod.EventBusSubscriber(modid = NFFGirls.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class NFFGirlsItemEventListeners
 {
 	@SubscribeEvent
-	public static void beforeRespawnerConstruct(NFFMobRespawnerConstructEvent.Before event)
+	public static void beforeRespawnerConstruct(NFFMobRespawnerBeforeConstructEvent event)
 	{
 	}
 	
 	@SubscribeEvent
-	public static void afterRespawnerConstruct(NFFMobRespawnerConstructEvent.After event)
+	public static void afterRespawnerConstruct(NFFMobRespawnerAfterConstructEvent event)
 	{
 		CompoundTag mobNbt = event.getRespawner().getMobNbt();
 		
@@ -77,7 +78,6 @@ public class NFFGirlsItemEventListeners
 		}
 	}
 
-	/** @deprecated Use {@link INFFGirlsTamed#getSharpnessModifierUUID} instead */
 	@Deprecated
 	protected static final UUID SHARPNESS_MODIFIER_UUID = UUID.fromString("9c12b503-63c0-43e6-bd30-d7aae9818c99");
 	
