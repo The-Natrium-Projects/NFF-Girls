cd69f9b408e3c19b75044151ffc0211dc2124539
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-03-13 01:21:26 +0800

    Ender Executor related
    Fixed ender executor keeping the attacking pose
    Now ender executor will not teleport on taming processing within 16 blocks instead of 7 blocks away from player

--- src/main/java/net/sodiumzh/nff/girls/entity/hmag/HmagEnderExecutorEntity.java
+++ src/main/java/net/sodiumzh/nff/girls/entity/hmag/HmagEnderExecutorEntity.java
@@ -175,7 +175,6 @@ public class HmagEnderExecutorEntity extends NFFTamedEnderManPreset implements I
 	}
 
 
-
 	// Interaction end
 
 	// No armor, hand items(0, 1), holding block(2) and 2 baubles(3, 4)
@@ -304,6 +303,7 @@ public class HmagEnderExecutorEntity extends NFFTamedEnderManPreset implements I
 			this.tickUpdateClientBeamAttackTime();
 		}
 		super.aiStep();
+		this.getEntityData().set(DATA_CREEPY, this.getTarget() != null);
 	}
 	
 	@Override
