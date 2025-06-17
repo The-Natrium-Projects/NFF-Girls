ca6da70955be556782be30ff6c6dfda4a7ec2beb
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-06-17 23:17:30 +0800

    Baubles and attributes
    Added some baubles (not all)
    Fixed wrong attribute implementations
    Still have an issue that the effects are not repeatable

--- src/main/java/net/sodiumzh/nff/girls/registry/NFFGirlsTags.java
+++ src/main/java/net/sodiumzh/nff/girls/registry/NFFGirlsTags.java
@@ -60,5 +60,7 @@ public class NFFGirlsTags
 		NFFGirls.MOD_ID, "neutral_on_high_progress");
 	public static final TagKey<EntityType<?>> AQUATIC_MOB = NFUTagStatics.createEntityTypeTag(
 		NFFGirls.MOD_ID, "aquatic_mob");
+	public static final TagKey<EntityType<?>> NETHER_MOB = NFUTagStatics.createEntityTypeTag(
+		NFFGirls.MOD_ID, "nether_mob");
 	
 }
