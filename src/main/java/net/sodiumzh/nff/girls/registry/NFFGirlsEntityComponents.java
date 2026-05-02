668c2979aa1cf6ddd511fd11cb51f72ef0122b13
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-05-02 19:55:25 +0800

    undead affinity handler
    wip

--- src/main/java/net/sodiumzh/nff/girls/registry/NFFGirlsEntityComponents.java
+++ src/main/java/net/sodiumzh/nff/girls/registry/NFFGirlsEntityComponents.java
@@ -1,8 +1,12 @@
 package net.sodiumzh.nff.girls.registry;
 
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobType;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.sodiumzh.nff.girls.NFFGirls;
 import net.sodiumzh.nfu.entity.anger.MobAngerHandlerComponent;
+import net.sodiumzh.nfu.entity.component.EntityComponentInitEvent;
 import net.sodiumzh.nfu.entity.component.EntityComponentType;
 import net.sodiumzh.nfu.entity.component.IEntityComponent;
 import net.sodiumzh.nfu.registry.NFURegistries;
@@ -16,6 +20,18 @@ public class NFFGirlsEntityComponents {
 
     public static final NFURegistry.Accessor<EntityComponentType<Mob, MobAngerHandlerComponent>> UNDEAD_AFFINITY_HANDLER =
         COLLECTION.register("undead_affinity_handler", () -> new EntityComponentType<>(Mob.class, MobAngerHandlerComponent.class,
-            MobAngerHandlerComponent::new))
+            m -> new MobAngerHandlerComponent(m, NFFGirlsAngerRules.UNDEAD_AFFINITY.get())));
+
+    @Mod.EventBusSubscriber(modid = NFFGirls.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class Attachment {
+
+        @SubscribeEvent
+        public static void attach(EntityComponentInitEvent event) {
+            if (event.getEntity() instanceof Mob mob && mob.getMobType().equals(MobType.UNDEAD))
+                event.getComponentManager().setRequired("/undead_affinity_handler", UNDEAD_AFFINITY_HANDLER.get());
+        }
+
+    }
+
 
 }
